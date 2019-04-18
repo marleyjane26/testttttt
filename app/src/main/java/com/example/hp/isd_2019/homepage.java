05aba69b88b064a9d7ddb2c4bd3d3b383341cdaf
ali remaity <aliremaitybook@gmail.com>
2019-04-19 00:29:16 +0300

    add login firebase athentcation

--- app/src/main/java/com/example/hp/isd_2019/homepage.java
+++ app/src/main/java/com/example/hp/isd_2019/homepage.java
@@ -1,9 +1,12 @@
 package com.example.hp.isd_2019;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,14 +16,88 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class homepage extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    TextView humdityText;
+    TextView tempText;
+    String humds;
+    String temps;
+    DatabaseReference dref = FirebaseDatabase.getInstance().getReference("DHT11");
+    Query lastQueryHumd = dref.child("humd").orderByKey().limitToLast(1);
+    Query lastQueryTemp = dref.child("temp").orderByKey().limitToLast(1);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        tempText = (TextView) findViewById(R.id.temp);
+        humdityText= (TextView) findViewById(R.id.humd);
+
+        lastQueryHumd.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                humds = dataSnapshot.getValue().toString();
+                String segments[] = humds.split("=");
+                String document = segments[segments.length - 1];
+                String last = document.replaceAll("[=}{]", "");
+
+                humdityText.setText(last);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w( "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+
+        lastQueryTemp.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                temps = dataSnapshot.getValue().toString();
+                String segments[] = temps.split("=");
+                String document = segments[segments.length - 1];
+                String last = document.replaceAll("[=}{]", "");
+
+                tempText.setText(last);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w( "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6),
+                new DataPoint(4, 6),
+                new DataPoint(4, 6),
+                new DataPoint(4, 6),
+                new DataPoint(4, 12)
+
+        });
+        series.setColor(Color.rgb(164, 66, 244));
+        series.setThickness(6);
+        series.setDrawBackground(true);
+        series.setBackgroundColor(Color.rgb(164, 66, 244));
+        series.setDrawDataPoints(true);
+        graph.addSeries(series);
+
+
        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
       setSupportActionBar(toolbar);
 
@@ -82,7 +159,7 @@ public class homepage extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            Intent my_intent= new Intent(homepage.this,paymentList.class);
+            Intent my_intent= new Intent(homepage.this,login.class);
             startActivity(my_intent);
         } else if (id == R.id.nav_gallery) {
             Intent my_intent= new Intent(homepage.this,Main2Activity.class);
@@ -91,11 +168,9 @@ public class homepage extends AppCompatActivity
             Intent my_intent= new Intent(homepage.this,MainActivity.class);
             startActivity(my_intent);
         } else if (id == R.id.nav_manage) {
-            Intent my_intent= new Intent(homepage.this,MainActivity.class);
-            startActivity(my_intent);
+
         } else if (id == R.id.nav_share) {
-            Intent my_intent= new Intent(homepage.this,MainActivity.class);
-            startActivity(my_intent);
+
         } else if (id == R.id.nav_send) {
 
         }
---
+++ app/src/main/java/com/example/hp/isd_2019/login.java
@@ -0,0 +1,102 @@
+package com.example.hp.isd_2019;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.concurrent.Executor;
+
+public class login extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    // UI references.
+    private EditText mEmail, mPassword;
+    private Button btnSignIn,btnSignOut,btnAddItems;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        btnSignIn = (Button) findViewById(R.id.email_sign_in_button);
+        btnSignOut = (Button) findViewById(R.id.email_sign_out_button);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                   // Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    toastMessage("Successfully signed in with: " + user.getEmail());
+                    Intent my_intent= new Intent(login.this,homepage.class);
+                    startActivity(my_intent);
+                } else {
+                    // User is signed out
+                  //2  Log.d(TAG, "onAuthStateChanged:signed_out");
+                    toastMessage("Successfully signed out.");
+                }
+                // ...
+            }
+        };
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = mEmail.getText().toString();
+                String pass = mPassword.getText().toString();
+                if(!email.equals("") && !pass.equals("")){
+                    mAuth.signInWithEmailAndPassword(email,pass);
+                }else{
+                    toastMessage("You didn't fill in all the fields.");
+                }
+            }
+        });
+
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                toastMessage("Signing Out...");
+            }
+        });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    //add a toast to show when successfully signed in
+    /**
+     * customizable toast
+     * @param message
+     */
+    private void toastMessage(String message){
+        Toast.makeText(this,message,Toast.LENGTH_SHORT).show();
+    }
+}
