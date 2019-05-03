649d29f489fd25080ffda9009acbe02825fc3de4
marleyjane26 <40754729+marleyjane26@users.noreply.github.com>
2019-05-03 10:33:23 +0300

    CCActivity edited

--- app/src/main/java/com/example/hp/isd_2019/creditcard.java
+++ app/src/main/java/com/example/hp/isd_2019/creditcard.java
@@ -1,62 +1,69 @@
 package com.example.hp.isd_2019;
 
 public class creditcard {
-    private   int cc_id;
+    private   String cc_id;
     private  String name_holder;
-    private int cc_number;
-    private int expire_date;
-    private double balance;
+    private String cc_number;
+    private String expire_date;
+    private String year;
+    private String balance;
+    private String Cvc;
 
 
-    public creditcard(int cc_id,int id_user,String name_holder,int cc_number,int expire_date,double balance){
-
-        this.cc_id=cc_id;
-
-        this.cc_number=cc_number;
-        this.name_holder=name_holder;
-        this.expire_date=expire_date;
-        this.balance=balance;
-    }
-    // Getter
-    public String getName_holder() {
-        return name_holder;
-    }
-    public int getCc_id() {
+    public String getCc_id() {
         return cc_id;
     }
 
-    public int getCc_number() {
+    public String getName_holder() {
+        return name_holder;
+    }
+
+    public String getCc_number() {
         return cc_number;
     }
 
-    public int getExpire_date() {
+    public String getExpire_date() {
         return expire_date;
     }
 
-    public double getBalance() {
+    public String getYear() {
+        return year;
+    }
+
+    public String getBalance() {
         return balance;
     }
 
-
-    public void setCc_id(int cc_id) {
-        this.cc_id = cc_id;
+    public String getCvc() {
+        return Cvc;
     }
 
+    public void setCc_id(String cc_id) {
+        this.cc_id = cc_id;
+    }
 
     public void setName_holder(String name_holder) {
         this.name_holder = name_holder;
     }
 
-    public void setCc_number(int cc_number) {
+    public void setCc_number(String cc_number) {
         this.cc_number = cc_number;
     }
 
-    public void setExpire_date(int expire_date) {
+    public void setExpire_date(String expire_date) {
         this.expire_date = expire_date;
     }
 
-    public void setBalance(double balance) {
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setBalance(String balance) {
         this.balance = balance;
+    }
+
+    public void setCvc(String cvc) {
+        Cvc = cvc;
     }
 }
 
--- app/src/main/java/com/example/hp/isd_2019/creditcardActivity.java
+++ app/src/main/java/com/example/hp/isd_2019/creditcardActivity.java
@@ -1,11 +1,32 @@
 package com.example.hp.isd_2019;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class creditcardActivity extends AppCompatActivity {
+
+    String currentuser = FirebaseAuth.getInstance().getCurrentUser().getUid();
+    private String URLstring = "https://lbpower.000webhostapp.com/api/getcc4one.php?fk_client="+currentuser;
     TextView ccNum,ccNam,exD,exY,ccvC;
+
+    private static ProgressDialog mProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +37,109 @@ public class creditcardActivity extends AppCompatActivity {
         exD=(TextView)findViewById(R.id.expDate);
         exY=(TextView)findViewById(R.id.year);
         ccvC=(TextView)findViewById(R.id.cvv);
+        retrieveJSON();
+    }
+    private void retrieveJSON() {
+
+        showSimpleProgressDialog(this, "Loading...","Please wait",false);
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, URLstring,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Log.d("strrrrr", ">>" + response);
+
+                        try {
+
+                            JSONObject obj = new JSONObject(response);
+
+
+                            JSONArray dataArray  = obj.getJSONArray("data");
+
+                            for (int i = 0; i < dataArray.length(); i++) {
+
+                                creditcard x = new creditcard();
+                                JSONObject dataobj = dataArray.getJSONObject(i);
+                                Log.d("strrrrr", ">>" + dataobj.getString("fname"));
+                                x.setCc_number(dataobj.getString("cc_number"));
+                                x.setName_holder(dataobj.getString("name_holder"));
+                                x.setExpire_date(dataobj.getString("expire_date"));
+                                x.setYear(dataobj.getString("year"));
+                                x.setCvc(dataobj.getString("cvc"));
+
+                                ccNum.setText(x.getCc_number());
+                                ccNam.setText(x.getName_holder());
+                                exD.setText(x.getExpire_date());
+                                exY.setText(x.getYear());
+                                ccvC.setText(x.getCvc());
+
+
+                                removeSimpleProgressDialog();
+
+                            }
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //displaying the error in toast if occurrs
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        requestQueue.add(stringRequest);
+
 
     }
+
+
+    public static void removeSimpleProgressDialog() {
+        try {
+            if (mProgressDialog != null) {
+                if (mProgressDialog.isShowing()) {
+                    mProgressDialog.dismiss();
+                    mProgressDialog = null;
+                }
+            }
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void showSimpleProgressDialog(Context context, String title,
+                                                String msg, boolean isCancelable) {
+        try {
+            if (mProgressDialog == null) {
+                mProgressDialog = ProgressDialog.show(context, title, msg);
+                mProgressDialog.setCancelable(isCancelable);
+            }
+
+            if (!mProgressDialog.isShowing()) {
+                mProgressDialog.show();
+            }
+
+        } catch (IllegalArgumentException ie) {
+            ie.printStackTrace();
+        } catch (RuntimeException re) {
+            re.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
