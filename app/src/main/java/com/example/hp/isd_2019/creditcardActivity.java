d7f4276cdc5ae5f63478abc1a0bf6726415a46e8
marleyjane26 <40754729+marleyjane26@users.noreply.github.com>
2019-05-03 09:45:09 +0300

    CCActivity

--- app/src/main/java/com/example/hp/isd_2019/creditcardActivity.java
+++ app/src/main/java/com/example/hp/isd_2019/creditcardActivity.java
@@ -2,12 +2,20 @@ package com.example.hp.isd_2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class creditcardActivity extends AppCompatActivity {
+    TextView ccNum,ccNam,exD,exY,ccvC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_creditcard);
+        setContentView(R.layout.activity_view_cc);
+        ccNum=(TextView)findViewById(R.id.ccnb);
+        ccNam=(TextView)findViewById(R.id.ccname);
+        exD=(TextView)findViewById(R.id.expDate);
+        exY=(TextView)findViewById(R.id.year);
+        ccvC=(TextView)findViewById(R.id.cvv);
+
     }
 }
